e46563b6f7c8b57a129c9dfe6bb1a4da24430ec3
Björn Breki <bjbreki@gmail.com>
2017-04-04 16:13:32 +0000

    Uppsetning á clasa söfnum
    Byrjuðum á að setja upp verkefniðog staðsetningu þess. Það er búið að setja upp clasa sem að mun halda utan íþróttarfélögin sem að verða í okkar útgáfu í topp trumps spilinu. Spennandi að halda áfram að vinna með þetta.

---
+++ lokaverkefni/lokaverkefni_for/lokaverkefni_for/Reglur.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lokaverkefni_for
+{
+    public class Reglur
+    {
+
+    }// endir á main
+}
