603f3d4b2c6e4257ed30080b17adc8eb957a8c45
Björn Breki <bjbreki@gmail.com>
2017-04-26 16:18:38 +0000

    Uppsetning á Keyrslu
    Keyrsla komin í gang þar sem að það er keyrt en það er með nokkrar villur

---
+++ lokaverkefni/keyrsla/Lokaverkefni_For_Keyrsla/Lokaverkefni_For_Keyrsla/Keyrsla.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lokaverkefni_for;
+
+namespace Lokaverkefni_For_Keyrsla
+{
+    class Keyrsla
+    {
+        /* Björn Breki og Róbert Atli
+         * Tækniskólinn
+         * Lokaverkefni Forritun
+         * 4 önn
+         * FOR2C3U
+         */
+        static void Main(string[] args)
+        {
+            Ithrottarlid lid_1 = new Ithrottarlid(7, 5, 8, "Þróttur");
+            Console.WriteLine(lid_1);
+            Console.ReadLine();
+        }// endir á main
+    }
+}
