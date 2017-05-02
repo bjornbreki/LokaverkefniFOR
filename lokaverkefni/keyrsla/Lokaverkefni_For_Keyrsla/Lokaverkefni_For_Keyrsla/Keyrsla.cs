4e8ea3118e88f3fb4e6d51e27644b7316c4f45f6
Björn Breki <bjbreki@gmail.com>
2017-05-02 16:44:52 +0000

    Leikurinn tilbúinn.
    Það er búið að setja upp á þann máta að hvert lið fær random tölu og þar með getur verið að Hrunumenn eru rosalega góðir í Handbolta eða stundum með einkun upp á 96 af 100. Það er ábyggilega erfitt að fynna betri uppsetningu, þar sem allt er keyrt eins hratt og hægt er og það er virkilega lítið af línum sem að verið er að vinna með. Þetta lítur rosalega vel út og spennandi að horfa upp á frammhaldið.

--- lokaverkefni/keyrsla/Lokaverkefni_For_Keyrsla/Lokaverkefni_For_Keyrsla/Keyrsla.cs
+++ lokaverkefni/keyrsla/Lokaverkefni_For_Keyrsla/Lokaverkefni_For_Keyrsla/Keyrsla.cs
@@ -17,17 +17,109 @@ namespace Lokaverkefni_For_Keyrsla
          */
         static void Main(string[] args)
         {
-            Ithrottarlid[] lid = new Ithrottarlid[7]; 
-            string[] nofn = new string[]{"Breiðablik", "HK", "KR", "Stjarnan", "Hamar", "Grótta", "FH"};
+            Ithrottarlid[] lid = new Ithrottarlid[45];
+            int[] Fotbolti = new int[45];
+            int[] Handbolti = new int[45];
+            int[] Korfubolti = new int[45];
+
+
+            string[] nofn = new string[]{"Afturelding", "Álftanes", "Ármann", "Björk", "Breidablik", "FH", "Fjölnir", "Fram", "FSU", "Fylkir", "Gerpla", "Gnupverjar", "Grindavík", "Grótta", "Grundafjordur",
+                "Hamar", "Hamrarnir", "Haukar", "HK", "Höttur", "Hrunamenn", "ÍA", "ÍBV", "IF Mílan", "ÍG", "ÍR", "Íþróttarfélagið Hörður", "Íþróttarfélag Breiðholts", "KR", "Leiknir", "Njardvík",
+                "Reykdælir", "Reynir Sandgerði", "Sindri", "Snæfell", "Stál Úlfur", "Stjarnan", "UMF Akureyri", "UMF Hekla", "UMF Kormákur", "UMF Þór", "Valur", "Volsungur", "Þór Akureyri", "Þróttur"};
             
             Random rand = new Random();
+
+            int tala1 = rand.Next(0, 46);
+            int tala2 = rand.Next(0, 46);
+
+
+            
             for (int i = 0; i < lid.Length; i++)
             {
-                lid[i] = new Ithrottarlid(rand.Next(50, 101), rand.Next(50, 101), rand.Next(50, 101), nofn[i]);
-                Console.WriteLine(lid[i]+"\n");
+                lid[i] = new Ithrottarlid(Fotbolti[i] = rand.Next(50, 101), Handbolti[i] = rand.Next(50, 101), Korfubolti[i] = rand.Next(50, 101), nofn[i]);
             }
-            
-            Console.ReadLine();
+
+
+                Console.WriteLine("Þetta er spilið þitt");
+                Console.WriteLine(lid[tala1] + "\n");
+                Console.WriteLine("Veldu 1,2 eða 3");
+                Console.WriteLine("1. Fótbolti");
+                Console.WriteLine("2. Handbolti");
+                Console.WriteLine("3. Körfubolti");
+                int val = Convert.ToInt32(Console.ReadLine());
+                if (val == 1)
+                {
+                    if (Fotbolti[tala1] > Fotbolti[tala2])
+                    {
+                        int stig = Fotbolti[tala1] - Fotbolti[tala2];
+                        Console.WriteLine("Þú vannst!");
+                        Console.WriteLine("Það munaði " + stig + " stigum!");
+                    }
+                    else if (Fotbolti[tala2] > Fotbolti[tala1])
+                    {
+                        int stig = Fotbolti[tala2] - Fotbolti[tala1];
+                        Console.WriteLine("Þú tapaðir!");
+                        Console.WriteLine("Það munaði " + stig + " stigum!");
+                    }
+                    else if (Fotbolti[tala1] == Fotbolti[tala2])
+                    {
+                        Console.WriteLine("Það var jafntefli!");
+                    }
+                    else
+	                {
+                        Console.WriteLine("Einhver villa.");
+	                }
+                }
+                else if (val == 2)
+                {
+                    if (Handbolti[tala1] > Handbolti[tala2])
+                    {
+                        int stig = Handbolti[tala1] - Handbolti[tala2];
+                        Console.WriteLine("Þú vannst!");
+                        Console.WriteLine("Það munaði " + stig + " stigum!");
+                    }
+                    else if (Handbolti[tala2] > Handbolti[tala1])
+                    {
+                        int stig = Handbolti[tala2] - Handbolti[tala1];
+                        Console.WriteLine("Þú tapaðir!");
+                        Console.WriteLine("Það munaði " + stig + " stigum!");
+                    }
+                    else if (Handbolti[tala1] == Handbolti[tala2])
+                    {
+                        Console.WriteLine("Það var jafntefli!");
+                    }
+                    else
+	                {
+                        Console.WriteLine("Einhver villa.");
+	                }
+                }
+                else if (val == 3)
+                {
+                    if (Korfubolti[tala1] > Korfubolti[tala2])
+                    {
+                        int stig = Korfubolti[tala1] - Korfubolti[tala2];
+                        Console.WriteLine("Þú vannst!");
+                        Console.WriteLine("Það munaði " + stig + " stigum!");
+                    }
+                    else if (Korfubolti[tala2] > Korfubolti[tala1])
+                    {
+                        int stig = Korfubolti[tala2] - Korfubolti[tala1];
+                        Console.WriteLine("Þú tapaðir!");
+                        Console.WriteLine("Það munaði " + stig + " stigum!");
+                    }
+                    else if (Korfubolti[tala1] == Korfubolti[tala2])
+                    {
+                        Console.WriteLine("Það var jafntefli!");
+                    }
+                    else
+	                {
+                        Console.WriteLine("Einhver villa.");
+	                }
+                }
+                
+                Console.WriteLine("Þetta er spilið hjá tölvuni");
+                Console.WriteLine(lid[tala2] + "\n");
+                Console.ReadLine();
         }// endir á main
     }
 }
